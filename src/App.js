118266d5869b7c0d96baabbecd232f7f57a2ad8d
isurojit <iamsurojit@hotmail.com>
2022-03-25 20:28:21 +0530

    empty template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import CounterHook from "./CounterHook";
-
-export const ThemeContext = React.createContext();
 
 const App = () => {
-  const [theme, setTheme] = useState("green");
-
-  const value = {
-    backgroundColor: theme,
-  };
-
-  const buttonBgChageHnadler = () => {
-    setTheme((prevTheme) => {
-      return prevTheme === "red" ? "blue" : "red";
-    });
-  };
-
-  return (
-    <ThemeContext.Provider value={value}>
-      <CounterHook initialValue={0} />
-      <button onClick={buttonBgChageHnadler}>Toggle Bg Color</button>
-    </ThemeContext.Provider>
-  );
+  <></>;
 };
 export default App;
