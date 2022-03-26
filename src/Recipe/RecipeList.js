1286886a926461ac87d8fd28b662f39768e1f8e3
isurojit <iamsurojit@hotmail.com>
2022-03-26 15:40:46 +0530

    added custom css

--- src/Recipe/RecipeList.js
+++ src/Recipe/RecipeList.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Recipe from "./Recipe";
+import "../Styling/recipelist.css";
 
 export default function RecipeList(props) {
   return (
-    <>
+    <div className="recipe-list">
       <div>
         {props.recipes.map((recipe) => {
           return <Recipe key={recipe.id} {...recipe} />;
         })}
       </div>
-      <button>Add Recipe</button>
-    </>
+      <div className="recipe-list__add-recipe-btn-container">
+        <button className="btn btn--primary">Add Recipe</button>
+      </div>
+    </div>
   );
 }
