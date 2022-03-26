aefaf8a35b0a43812e58e60435a5ae1cdd7330f4
isurojit <iamsurojit@hotmail.com>
2022-03-26 10:57:30 +0530

    ingredients added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+
 import RecipeList from "./Recipe/RecipeList";
 
 const App = () => {
@@ -18,6 +19,28 @@ const sampleRecipeList = [
     cookTime: "15:00",
     instructions:
       " 1. Clean the Chicken. \n 2. Marinate overnight with salt, pepper, oilve oil & chilly spices. \n3. Grill it for 160c for 10 minutes. \n4. Serve it with wine.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Chicken",
+        amount: "800 gm",
+      },
+      {
+        id: 2,
+        name: "Salt",
+        amount: "20 gm",
+      },
+      {
+        id: 3,
+        name: "Pepper",
+        amount: "30 gm",
+      },
+      {
+        id: 4,
+        name: "Butter",
+        amount: "50 gm",
+      },
+    ],
   },
   {
     id: 2,
@@ -26,6 +49,28 @@ const sampleRecipeList = [
     cookTime: "25:00",
     instructions:
       " 1. Make a flour dough & seal it with dry cloth for 4 hrs. \n 2. Make an big round shape of that dough. \n3. Add pizza sauce, Chicken bbq & other ingredients. \n4. Put it on Oven for 15:00 at 200c. \n5. Slice the piza in multiple rectangle shape & serve it with wine.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Chicken",
+        amount: "200 gm",
+      },
+      {
+        id: 2,
+        name: "Flour",
+        amount: "200 gm",
+      },
+      {
+        id: 3,
+        name: "Pizza Sauce",
+        amount: "50 gm",
+      },
+      {
+        id: 5,
+        name: "Veggies",
+        amount: "100 gm",
+      },
+    ],
   },
 ];
 export default App;
---
+++ src/Recipe/IngredientList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Ingredients from "./Ingredients";
+
+export default function IngredientList({ ingredients }) {
+  const ingredientElements = ingredients.map((ingredient) => {
+    return <Ingredients key={ingredient.id} {...ingredient} />;
+  });
+  return <div>{ingredientElements}</div>;
+}
